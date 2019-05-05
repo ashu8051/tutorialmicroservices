6f93939d226d9450beaf1037bd89dd50e3b3ce24
Ashutosh Choubey <ashu8051@gmail.com>
2019-05-05 17:21:35 +0530

    First Commit IamConfig

---
+++ IamiConfigServer/src/main/java/com/example/demo/IamiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamiConfigServerApplication.class, args);
+	}
+
+}
